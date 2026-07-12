2175b590cbfdacfd1dc4287977506177c9ddf9b7
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:18:42 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "machine_learning_workspace_network_outbound_rule_private_endpoints" {
-  description = "All machine_learning_workspace_network_outbound_rule_private_endpoint resources"
-  value       = azurerm_machine_learning_workspace_network_outbound_rule_private_endpoint.machine_learning_workspace_network_outbound_rule_private_endpoints
-}
 output "machine_learning_workspace_network_outbound_rule_private_endpoints_name" {
-  description = "List of name values across all machine_learning_workspace_network_outbound_rule_private_endpoints"
-  value       = [for k, v in azurerm_machine_learning_workspace_network_outbound_rule_private_endpoint.machine_learning_workspace_network_outbound_rule_private_endpoints : v.name]
+  description = "Map of name values across all machine_learning_workspace_network_outbound_rule_private_endpoints, keyed the same as var.machine_learning_workspace_network_outbound_rule_private_endpoints"
+  value       = { for k, v in azurerm_machine_learning_workspace_network_outbound_rule_private_endpoint.machine_learning_workspace_network_outbound_rule_private_endpoints : k => v.name }
 }
 output "machine_learning_workspace_network_outbound_rule_private_endpoints_service_resource_id" {
-  description = "List of service_resource_id values across all machine_learning_workspace_network_outbound_rule_private_endpoints"
-  value       = [for k, v in azurerm_machine_learning_workspace_network_outbound_rule_private_endpoint.machine_learning_workspace_network_outbound_rule_private_endpoints : v.service_resource_id]
+  description = "Map of service_resource_id values across all machine_learning_workspace_network_outbound_rule_private_endpoints, keyed the same as var.machine_learning_workspace_network_outbound_rule_private_endpoints"
+  value       = { for k, v in azurerm_machine_learning_workspace_network_outbound_rule_private_endpoint.machine_learning_workspace_network_outbound_rule_private_endpoints : k => v.service_resource_id }
 }
 output "machine_learning_workspace_network_outbound_rule_private_endpoints_spark_enabled" {
-  description = "List of spark_enabled values across all machine_learning_workspace_network_outbound_rule_private_endpoints"
-  value       = [for k, v in azurerm_machine_learning_workspace_network_outbound_rule_private_endpoint.machine_learning_workspace_network_outbound_rule_private_endpoints : v.spark_enabled]
+  description = "Map of spark_enabled values across all machine_learning_workspace_network_outbound_rule_private_endpoints, keyed the same as var.machine_learning_workspace_network_outbound_rule_private_endpoints"
+  value       = { for k, v in azurerm_machine_learning_workspace_network_outbound_rule_private_endpoint.machine_learning_workspace_network_outbound_rule_private_endpoints : k => v.spark_enabled }
 }
 output "machine_learning_workspace_network_outbound_rule_private_endpoints_sub_resource_target" {
-  description = "List of sub_resource_target values across all machine_learning_workspace_network_outbound_rule_private_endpoints"
-  value       = [for k, v in azurerm_machine_learning_workspace_network_outbound_rule_private_endpoint.machine_learning_workspace_network_outbound_rule_private_endpoints : v.sub_resource_target]
+  description = "Map of sub_resource_target values across all machine_learning_workspace_network_outbound_rule_private_endpoints, keyed the same as var.machine_learning_workspace_network_outbound_rule_private_endpoints"
+  value       = { for k, v in azurerm_machine_learning_workspace_network_outbound_rule_private_endpoint.machine_learning_workspace_network_outbound_rule_private_endpoints : k => v.sub_resource_target }
 }
 output "machine_learning_workspace_network_outbound_rule_private_endpoints_workspace_id" {
-  description = "List of workspace_id values across all machine_learning_workspace_network_outbound_rule_private_endpoints"
-  value       = [for k, v in azurerm_machine_learning_workspace_network_outbound_rule_private_endpoint.machine_learning_workspace_network_outbound_rule_private_endpoints : v.workspace_id]
+  description = "Map of workspace_id values across all machine_learning_workspace_network_outbound_rule_private_endpoints, keyed the same as var.machine_learning_workspace_network_outbound_rule_private_endpoints"
+  value       = { for k, v in azurerm_machine_learning_workspace_network_outbound_rule_private_endpoint.machine_learning_workspace_network_outbound_rule_private_endpoints : k => v.workspace_id }
 }
 
